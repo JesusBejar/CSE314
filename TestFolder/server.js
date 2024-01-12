80febf8e64c6d5df121a1f68bac324547d140507
Jesus Bejar <israelbejarii@gmail.com>
2024-01-12 14:50:18 -0700

    ...

--- TestFolder/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const connectDB = require('./DBConnection/connection');
-
-connectDB();
-app.use('/api/userModel', require('./user.js'));
-const port = process.env.port || 3000;
-
-app.listen(port, () => console.log('Server has begun'));
